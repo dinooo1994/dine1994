d7ed3b8f70ace9990db9807f4d32bbd26b8a5111
dinooo1994 <157990311+dinooo1994@users.noreply.github.com>
2024-02-04 00:22:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def click_on_elements(driver):
         # first_element_to_click = driver.find_element(By.CSS_SELECTOR, '.ship-to--menuItem--WdBDsYl')
         action = ActionChains(driver)
         action.move_to_element(first_element_to_click).click().perform()
-        time.sleep(0.5)
+        time.sleep(5)
         second_element_to_click = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, '.es--saveBtn--w8EuBuy'))
         )
@@ -76,9 +76,9 @@ def scrape_and_display(product_url):
     total_value = 0 
     try:
         driver.get(product_url)
-        time.sleep(0.5)
+        time.sleep(5)
         click_on_elements(driver)
-        # time.sleep(0.5)
+        time.sleep(5)
         xpath = '//img[@class="price-banner--slogan--SlQzWHE pdp-comp-banner-slogan"]'
         image_elements = driver.find_elements(By.XPATH, xpath)
         target_image_url = "https://ae01.alicdn.com/kf/Sabdabe1e0ed84a179ab6c06fc9f316769/380x144.png_.webp"
