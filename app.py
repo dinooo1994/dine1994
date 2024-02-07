139521afe4b546b7182ac0a6f4e3759e474264cd
dinooo1994 <157990311+dinooo1994@users.noreply.github.com>
2024-02-07 14:50:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,15 +18,15 @@ def click_on_elements(driver):
         first_element_xpath = '//*[@id="_full_container_header_23_"]/div[2]/div/div[2]/div[2]'
         first_element = driver.find_element(By.XPATH, first_element_xpath)
         ActionChains(driver).move_to_element(first_element).click().perform()
-        time.sleep(0.5)  
+        time.sleep(1)  
         second_element_xpath = '//*[@id="_full_container_header_23_"]/div[2]/div/div[2]/div[2]/div[2]/div[2]/div'
         second_element = driver.find_element(By.XPATH, second_element_xpath)
         ActionChains(driver).move_to_element(second_element).click().perform()
-        time.sleep(0.5)
+        time.sleep(1)
         third_element_xpath = '//*[@id="_full_container_header_23_"]/div[2]/div/div[2]/div[2]/div[2]/div[2]/div/div[2]/div[56]'
         third_element = driver.find_element(By.XPATH, third_element_xpath)
         ActionChains(driver).move_to_element(third_element).click().perform()
-        time.sleep(0.5)
+        time.sleep(1)
         fourth_element_xpath = '//*[@id="_full_container_header_23_"]/div[2]/div/div[2]/div[2]/div[2]/div[7]'
         fourth_element = driver.find_element(By.XPATH, fourth_element_xpath)
         ActionChains(driver).move_to_element(fourth_element).click().perform()
