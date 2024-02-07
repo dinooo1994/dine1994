e6a2811c717d9a9c355f78101adfc0675c82095a
dinooo1994 <157990311+dinooo1994@users.noreply.github.com>
2024-02-07 11:34:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,39 +20,23 @@ def click_on_elements(driver):
         )
         # first_element_to_click = driver.find_element(By.CSS_SELECTOR, '.ship-to--menuItem--WdBDsYl')
         action = ActionChains(driver)
-        action.move_to_element(first_element_to_click).click().perform()        
+        action.move_to_element(first_element_to_click).click().perform()
         time.sleep(5)
-
-        third_element_to_click = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, '.select--text--1b85oDo'))
-        )
-        action = ActionChains(driver)
-        action.move_to_element(third_element_to_click).click().perform()
-        time.sleep(5)
-        
-        fourth_element_to_click = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, '.select--item--32FADYB'))
-        )
-        action = ActionChains(driver)
-        action.move_to_element(fourth_element_to_click).click().perform()
-        time.sleep(5)
-
-        second_element_to_click = WebDriverWait(driver, 20).until(
+        second_element_to_click = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, '.es--saveBtn--w8EuBuy'))
         )
         # second_element_to_click = driver.find_element(By.CSS_SELECTOR, '.es--saveBtn--w8EuBuy')
         action = ActionChains(driver)
         action.move_to_element(second_element_to_click).click().perform()
+        time.sleep(5)
     except Exception as e:
         print(f"Error clicking on the specified elements: {e}")
-
 def extract_numerical_value(text):
     try:
         return float(''.join(c for c in text if c.isdigit() or c in ['.', ',']))
     except ValueError:
         print("Error!! in extract_numerical_value function...")
         return 0
-
 def scrape_and_display(product_url):
     print("============Start Selenuim==========")
     chrome_options = webdriver.ChromeOptions()
@@ -78,7 +62,6 @@ def scrape_and_display(product_url):
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument('--allow-running-insecure-content')
     chrome_options.add_argument("--disable-dev-shm-usage")
-    # chrome_options.add_argument('--lang=de-DE')
     # user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.6167.85 Safari/537.36'
     # chrome_options.add_argument(f'user-agent={user_agent}')
     driver = webdriver.Chrome(service=service, options=chrome_options)
@@ -94,9 +77,9 @@ def scrape_and_display(product_url):
     total_value = 0 
     try:
         driver.get(product_url)
-        time.sleep(0.1)
+        time.sleep(0.5)
         click_on_elements(driver)
-        time.sleep(1)
+        # time.sleep(0.5)
         xpath = '//img[@class="price-banner--slogan--SlQzWHE pdp-comp-banner-slogan"]'
         image_elements = driver.find_elements(By.XPATH, xpath)
         target_image_url = "https://ae01.alicdn.com/kf/Sabdabe1e0ed84a179ab6c06fc9f316769/380x144.png_.webp"
@@ -108,9 +91,6 @@ def scrape_and_display(product_url):
         soup = BeautifulSoup(page_source, 'html.parser')
         product_price_element = soup.find('div', class_='es--wrap--erdmPRe')
         product_price_text = product_price_element.text.strip().replace('€', '') if product_price_element else 'Not found'
-        # print("=================product_price_text============")
-        # print(product_price_text, product_price_element.text)
-        # print("===============================")
         shipping_price_element = soup.find('div', class_='dynamic-shipping-titleLayout')
         shipping_price_text = shipping_price_element.text.strip().replace('€', '') if shipping_price_element else 'Not found'
         relevant_shipping_info = shipping_price_text.split(':')[-1].strip()
