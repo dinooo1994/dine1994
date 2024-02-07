8b23dd8ab9c5aafe2a4fd16bc1db4443cb8ba838
dinooo1994 <157990311+dinooo1994@users.noreply.github.com>
2024-02-07 14:47:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def scrape_and_display(product_url):
     # chrome_options.add_argument("--proxy-server='direct://'")
     # chrome_options.add_argument("--proxy-bypass-list=*")
     # chrome_options.add_argument("--start-maximized")
-    #chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--ignore-certificate-errors')
