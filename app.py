8dd563047b577318059edccab4e3f31ffcb64048
dinooo1994 <157990311+dinooo1994@users.noreply.github.com>
2024-02-18 21:16:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,72 +1,32 @@
 from flask import Flask, render_template, request
 from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-
 from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
-# from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-# import os 
 import time
 
 app = Flask(__name__)
 def click_on_elements(driver):
     try:
-        # Wait for the element to be present
-        first_element_to_click = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, '.ship-to--menuItem--WdBDsYl'))
-        )
-        # first_element_to_click = driver.find_element(By.CSS_SELECTOR, '.ship-to--menuItem--WdBDsYl')
+        first_element_to_click = driver.find_element(By.CSS_SELECTOR, '.ship-to--menuItem--WdBDsYl')
         action = ActionChains(driver)
-        action.move_to_element(first_element_to_click).click().perform()        
-        time.sleep(3)
-        second_element_to_click = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, '.es--saveBtn--w8EuBuy'))
-        )
-        # second_element_to_click = driver.find_element(By.CSS_SELECTOR, '.es--saveBtn--w8EuBuy')
+        action.move_to_element(first_element_to_click).click().perform()
+        time.sleep(1)
+        second_element_to_click = driver.find_element(By.CSS_SELECTOR, '.es--saveBtn--w8EuBuy')
         action = ActionChains(driver)
         action.move_to_element(second_element_to_click).click().perform()
     except Exception as e:
         print(f"Error clicking on the specified elements: {e}")
-
 def extract_numerical_value(text):
     try:
         return float(''.join(c for c in text if c.isdigit() or c in ['.', ',']))
     except ValueError:
-        print("Error!! in extract_numerical_value function...")
         return 0
-
 def scrape_and_display(product_url):
-    print("============Start Selenuim==========")
     chrome_options = webdriver.ChromeOptions()
-    # print("Current directory:", os.getcwd())
-    # print("List files in the current directory:", os.listdir())
-    # drivers_directory = "./drivers"
-    # files_in_drivers = os.listdir(drivers_directory)
-    # print("List files in the 'drivers' directory:", files_in_drivers)
-    # chrome_options.binary_location = os.environ.get("/usr/bin/google-chrome")
-    # chrome_driver_path = ChromeDriverManager().install()
-    chrome_driver_path = "drivers/chromedriver"
-    service = webdriver.ChromeService(executable_path= chrome_driver_path)
-    
-    # service = webdriver.ChromeService(executable_path= ChromeDriverManager().install())
-    # chrome_options.add_argument("--window-size=1920,1080")
-    # chrome_options.add_argument("--disable-extensions")
-    # chrome_options.add_argument("--proxy-server='direct://'")
-    # chrome_options.add_argument("--proxy-bypass-list=*")
-    # chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--ignore-certificate-errors')
-    chrome_options.add_argument('--allow-running-insecure-content')
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    # chrome_options.add_argument('--lang=de-DE')
-    # user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.6167.85 Safari/537.36'
-    # chrome_options.add_argument(f'user-agent={user_agent}')
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     result = {
         "result_text": "",
         "price": 0,
@@ -74,12 +34,8 @@ def scrape_and_display(product_url):
         "total": 0,
         "get_day": 0  
     }
-    product_price = 0
-    shipping_price = 0
-    total_value = 0 
     try:
         driver.get(product_url)
-        time.sleep(0.5)
         click_on_elements(driver)
         time.sleep(1)
         xpath = '//img[@class="price-banner--slogan--SlQzWHE pdp-comp-banner-slogan"]'
@@ -93,9 +49,6 @@ def scrape_and_display(product_url):
         soup = BeautifulSoup(page_source, 'html.parser')
         product_price_element = soup.find('div', class_='es--wrap--erdmPRe')
         product_price_text = product_price_element.text.strip().replace('€', '') if product_price_element else 'Not found'
-        # print("=================product_price_text============")
-        # print(product_price_text, product_price_element.text)
-        # print("===============================")
         shipping_price_element = soup.find('div', class_='dynamic-shipping-titleLayout')
         shipping_price_text = shipping_price_element.text.strip().replace('€', '') if shipping_price_element else 'Not found'
         relevant_shipping_info = shipping_price_text.split(':')[-1].strip()
@@ -190,4 +143,4 @@ def index():
         return render_template('index.html', result=result)
     return render_template('index.html', result=None)
 if __name__ == '__main__':
-    app.run(debug=False, threaded=True)
+    app.run(debug=True) 
