579299aea8daea8d86830173af117b9c22220651
dinooo1994 <157990311+dinooo1994@users.noreply.github.com>
2024-02-19 23:05:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -65,9 +65,7 @@ def scrape_and_display(product_url):
     chrome_options.add_argument("--enable-features=AllowGeolocationOnInsecureOrigins")
     chrome_options.add_argument("--no-sandbox")
 
-
-# إنشاء مشغل متصفح Chrome مع الخيارات المعطاة
-    driver = webdriver.Chrome(executable_path=chrome_driver_path, chrome_options=chrome_options)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # تحديد الموقع الجغرافي المفضل للجزائر باستخدام الجافا سكريبت
     driver.execute_cdp_cmd("Emulation.setGeolocationOverride", {
@@ -78,7 +76,7 @@ def scrape_and_display(product_url):
     # user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.6167.85 Safari/537.36'
     # chrome_options.add_argument(f'user-agent={user_agent}')
     #driver = webdriver.Chrome(executable_path=chrome_driver_path, chrome_options=chrome_options)
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    
     result = {
         "result_text": "",
         "price": 0,
